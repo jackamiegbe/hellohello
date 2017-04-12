0e584e877b398c686b2968aa11e971ba8d9c36b2
git@github.com:jackamiegbe/jackattack.git <you@example.com>
2017-04-12 18:27:33 +0100

    Initial Commit

---
+++ hellohello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hellohello
+//
+//  Created by MACBOOK on 12/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
