5b52aaa5c46aafbc8cb03e544c91034bf17a8d9a
git@github.com:jackamiegbe/jackattack.git <you@example.com>
2017-04-12 18:36:35 +0100

    Eat a cow
    yeah yeah

--- hellohello/ViewController.swift
+++ hellohello/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("hello world! whats good")
+        print("eat a cow")
     }
 
     override func didReceiveMemoryWarning() {
